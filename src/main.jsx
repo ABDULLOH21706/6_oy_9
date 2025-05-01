0c8ab6855927254a6d3d594f930b2a33603e8036
mrzayev_abdulloh <abdullohmirzayev841@gmail.com>
2025-05-01 16:01:06 +0500

    ozgardi

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./home/index.jsx";
-import Shop from "./shop/index.jsx";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "../routes/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
     <Home />
-    <Shop />
   </StrictMode>
 );
